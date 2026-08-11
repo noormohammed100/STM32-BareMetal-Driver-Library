2c0e9e035103d4420a97caecdd65f63d3ad35b6c
noormohammed100 <noorkhanfeb1@gmail.com>
2026-08-11 07:13:01 +0100

    feat: add struct-based LED toggle project

---
+++ 7_uart_with_function/Inc/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ *  Created on: 3 Aug 2026
+ *      Author: noork
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+void uart2_tx_init (void);
+
+#endif /* UART_H_ */
